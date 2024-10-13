dcdd8675b446b2f8ec9acc2d4f2a86c3db6ebd96
Ali Hashemi <seyedALIhash1@gmail.com>
2024-10-13 21:40:25 +0330

    starting project

---
+++ tracking/app_settings.py
@@ -0,0 +1,26 @@
+class AppSettings(object):
+    def __init__(self, prefix):
+        self.prefix = prefix
+
+    def _setting(self, name, dflt):
+        from django.conf import settings
+
+        return getattr(settings, self.prefix + name, dflt)
+
+    @property
+    def DECODE_REQUEST_BODY(self):
+        """
+        Allow the request.body byte string to be decoded to a string.
+
+        If you are allowing large file uploads, setting this to False prevents
+        a RequestDataTooBig exception.
+        """
+        return self._setting("DECODE_REQUEST_BODY", True)
+
+    @property
+    def PATH_LENGTH(self):
+        """Maximum length of request path to log"""
+        return self._setting("PATH_LENGTH", 200)
+
+
+app_settings = AppSettings("DRF_TRACKING_")
